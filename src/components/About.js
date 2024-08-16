661e6f6078d9e91a968d8598db3413c12de0a978
cgp-client <code.gen.pro.system@gmail.com>
2024-08-16 10:22:18 +0300

    Adding src/components/About.js

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export function About() {
+  return (
+    <div className="about-section">
+      <h1>About</h1>
+      <p>This is the About section.</p>
+    </div>
+  );
+}
+
+export default About;
