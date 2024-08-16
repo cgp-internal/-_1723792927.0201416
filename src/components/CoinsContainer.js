b7923a8916cfc95872bbd06cc351d355cee46920
cgp-client <code.gen.pro.system@gmail.com>
2024-08-16 10:22:20 +0300

    Adding src/components/CoinsContainer.js

---
+++ src/components/CoinsContainer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export const CoinsContainerTitle = () => <h2>Coins Container</h2>;
+
+export const CoinsContainerDescription = () => <p>This is a placeholder for future content.</p>;
+
+const CoinsContainer = () => {
+  return (
+    <div>
+      <CoinsContainerTitle />
+      <CoinsContainerDescription />
+    </div>
+  );
+};
+
+export default CoinsContainer;
