bbecfe16877649c3c460899b280a8ff4bf5d6460
cgp-client <code.gen.pro.system@gmail.com>
2024-08-16 10:22:21 +0300

    Adding src/components/LiveReports.js

---
+++ src/components/LiveReports.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export const LiveReportsTitle = () => <h1>Live Reports</h1>;
+
+const LiveReports = () => {
+  return (
+    <div>
+      <LiveReportsTitle />
+      {/* Add Live Reports content here */}
+    </div>
+  );
+};
+
+export { LiveReports };
